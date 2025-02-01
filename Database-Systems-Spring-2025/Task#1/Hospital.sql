296ed19da6fb0cda139aa9f2239b4bc70805642e
Muhammad Saad <165696604+saad-waseem7@users.noreply.github.com>
2025-02-01 09:03:33 +0500

    Add files via upload

---
+++ Database-Systems-Spring-2025/Task#1/Hospital.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE Hospital;
+
+USE Hospital;
+
+CREATE TABLE Doctor (
+    DoctorID INT PRIMARY KEY,
+    Name VARCHAR(70),
+    Age INT CHECK (Age BETWEEN 25 AND 100),
+    Specialization VARCHAR(70)
+);
+
+CREATE TABLE Patient (
+    PatientID INT PRIMARY KEY,
+    Name VARCHAR(70),
+    Age INT,
+    DoctorID INT,
+    FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID)
+);
+
+ALTER TABLE Patient
+    ADD CONSTRAINT FK_DoctorID FOREIGN KEY (DoctorID)
+    REFERENCES Doctor(DoctorID)
+    ON DELETE CASCADE
+    ON UPDATE SET NULL;
