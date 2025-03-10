8266ac3f53a0622c4a75a942955761bdbd37421f
Muhammad Saad <165696604+saad-waseem7@users.noreply.github.com>
2025-03-10 14:07:28 +0500

    EDIT

--- Database-Systems-Spring-2025/Lab#5/L5.sql
+++ Database-Systems-Spring-2025/Lab#5/L5.sql
@@ -1,7 +1,6 @@
 Create database twitter
 use twitter
 
-
 create table Users
 (
 User_ID int,
@@ -20,14 +19,12 @@ INSERT dbo.users (User_ID,UserName, Age, Country,privacy) VALUES (6,N'Natasha', 
 INSERT dbo.users (User_ID,UserName, Age, Country,privacy) VALUES (7,N'Samuel', 37, N'Australia',0)
 INSERT dbo.users (User_ID,UserName, Age, Country,privacy) VALUES (8,N'Sara', 30, N'Iran',1)
 
-
 create table Following
 (
 FollowerUserName varchar(20) Foreign key References Users(UserName) ,
 FollowedUserName varchar(20) Foreign key References Users(UserName),
 primary key (FollowerUserName, FollowedUserName)
 )
-
 
 INSERT dbo.Following (FollowerUserName, FollowedUserName) VALUES (N'Ali123', N'DonaldTrump')
 INSERT dbo.Following (FollowerUserName, FollowedUserName) VALUES (N'Aliza', N'Ali123')
@@ -41,7 +38,6 @@ INSERT dbo.Following (FollowerUserName, FollowedUserName) VALUES (N'Samuel', N'D
 INSERT dbo.Following (FollowerUserName, FollowedUserName) VALUES (N'Samuel', N'ImranKhan')
 INSERT dbo.Following (FollowerUserName, FollowedUserName) VALUES (N'Sara', N'DonaldTrump')
 
-
 Create table Tweets
 (
 TweetID int primary key,
@@ -49,21 +45,20 @@ UserName varchar(20) Foreign key References Users(UserName),
 Text varchar(140)
 )
 
-
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (1, N'Ali123', N'Pakistan’s largest-ever population #Census  starts today in 63 districts after 19 years. ')
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (2, N'ImranKhan', N'Pakistan’s largest-ever population #Census  starts today in 63 districts after 19 years. ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (1, N'Ali123', N'Pakistanï¿½s largest-ever population #Census  starts today in 63 districts after 19 years. ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (2, N'ImranKhan', N'Pakistanï¿½s largest-ever population #Census  starts today in 63 districts after 19 years. ')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (3, N'Sara', N'Take the soldier who come to our door for #Census as A ThankYou from Pakistan Army for our devotion. #CensusWithSupportOfArmy')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (4, N'ImranKhan', N'Pakistan is ing to experience 6th #Census 2017.')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (5, N'Sara', N'Only #census can reveal where this aunty lives ')
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (6, N'ImranKhan', N'Quand tu te lèves 3h en avance (littéralement) pour la #massecritique ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (6, N'ImranKhan', N'Quand tu te lï¿½ves 3h en avance (littï¿½ralement) pour la #massecritique ')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (7, N'Sara', N'Over 1 million decrypted Gmail and Yahoo accounts allegedly up for sale on the Dark Web https://geekz0ne.fr/shaarli/?wQRSoQ  #piratage')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (8, N'Sara', N'Harry pot-head and the Sorcerer''s stoned')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (9, N'DonaldTrump', N'LSDespicable Me  #DrugMovies')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (10, N'ImranKhan', N'Forrest Bump #DrugMovies @midnight')
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (12, N'DonaldTrump', N'Quand tu te lèves 3h en avance (littéralement) pour la #massecritique ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (12, N'DonaldTrump', N'Quand tu te lï¿½ves 3h en avance (littï¿½ralement) pour la #massecritique ')
 INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (13, N'Sara', N'#DrugMovies')
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (14, N'DonaldTrump', N'Quand tu te lèves 3h en avance (littéralement) pour la #massecritique ')
-INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (15, N'Aliza', N'Quand tu te lèves 3h en avance (littéralement) pour la #massecritique ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (14, N'DonaldTrump', N'Quand tu te lï¿½ves 3h en avance (littï¿½ralement) pour la #massecritique ')
+INSERT dbo.Tweets (TweetID, UserName, Text) VALUES (15, N'Aliza', N'Quand tu te lï¿½ves 3h en avance (littï¿½ralement) pour la #massecritique ')
 
 Create table Likes
 (
@@ -76,13 +71,11 @@ primary key (TweetID ,LikeByUserName)
 INSERT dbo.Likes (TweetID, LikeByUserName, LikeOnDate) VALUES (1, N'Aliza', CAST(0x693C0B00 AS Date))
 INSERT dbo.Likes (TweetID, LikeByUserName, LikeOnDate) VALUES (2, N'Aliza', CAST(0x693C0B00 AS Date))
 
-
 Create table Interests
 (
 InterestID int primary key,
 Description varchar(30)
 )
-
 
 INSERT dbo.Interests (InterestID, Description) VALUES (10, N'Politics')
 INSERT dbo.Interests (InterestID, Description) VALUES (11, N'Sports')
@@ -90,14 +83,12 @@ INSERT dbo.Interests (InterestID, Description) VALUES (12, N'Movies')
 INSERT dbo.Interests (InterestID, Description) VALUES (13, N'Education')
 INSERT dbo.Interests (InterestID, Description) VALUES (14, N'Video Games')
 
-
-
 create table UserInterests
 (UserName varchar(20)  Foreign key References Users(UserName),
 InterestID int Foreign key References Interests(InterestID)
 primary key (UserName, InterestID)
 )
- 
+
 INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'Ali123', 10)
 INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'Ali123', 11)
 INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'Ali123', 13)
@@ -108,12 +99,10 @@ INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'Aliza', 13)
 INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'Aliza', 14)
 INSERT dbo.UserInterests (UserName, InterestID) VALUES (N'ImranKhan', 10)
  
- 
 Create table Hashtags
 (HashtagID int primary key,
 Hashtag varchar(30))
- 
- 
+  
 INSERT dbo.Hashtags (HashtagID, Hashtag) VALUES (1, N'#Census')
 INSERT dbo.Hashtags (HashtagID, Hashtag) VALUES (2, N'#ClassiqueMatin')
 INSERT dbo.Hashtags (HashtagID, Hashtag) VALUES (3, N'#MasseCritique')
@@ -126,7 +115,6 @@ Select * from Interests
 Select * from Tweets
 Select * from UserInterests
 Select * from Users
-
 
 --7. Retrieve tweets by users who have at least one interest in "Politics"
 select t.* from Tweets t
@@ -170,7 +158,7 @@ select t.TweetID, t.Text  from Tweets t
 join Hashtags h on t.Text like '%' + h.Hashtag + '%'
 where h.HashtagID = (select top 1 HashtagID from Hashtags group by HashtagID order by count(*) desc);
 
--- 15. Find users who have liked tweets from users they don’t follow
+-- 15. Find users who have liked tweets from users they donï¿½t follow
 select distinct l.LikeByUserName from Likes l
 join Tweets t on l.TweetID = t.TweetID
 left join Following f on l.LikeByUserName = f.FollowerUserName and t.UserName = f.FollowedUserName
